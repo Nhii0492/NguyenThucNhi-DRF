c5cde623930657289877e8a89f7db4904efcb50a
Nhii0492 <Nhinguyen0492@gmail.com>
2024-09-27 12:26:09 +0700

    tutorial5

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,20 +2,25 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    question_text = models.CharField(max_length=200, help_text="Enter the question text.")
+    pub_date = models.DateTimeField("date published", help_text="Enter the date when the question was published.")
+
+    class Meta:
+        ordering = ['-pub_date']
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        """Returns True if the question was published within the last day."""
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return self.pub_date >= now - datetime.timedelta(days=1) and self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, help_text="Enter the choice text.")
     votes = models.IntegerField(default=0)
 
     def __str__(self):
